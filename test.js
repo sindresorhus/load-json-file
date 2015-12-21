724b188f5ceafa3ea0d9d6bbac5d315faba8689a
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-21 17:16:01 +0100

    remove now moot XO ignore

--- test.js
+++ test.js
@@ -6,11 +6,9 @@ const fixture = path.join(__dirname, 'package.json');
 
 test('async', async t => {
 	const data = await fn(fixture);
-
 	t.is(data.name, 'load-json-file');
 });
 
 test('sync', t => {
 	t.is(fn.sync(fixture).name, 'load-json-file');
-	t.end();
 });
