5772916b65116f8bc28f6a9baf278d406f3d031f
Sindre Sorhus <sindresorhus@gmail.com>
2016-05-01 00:16:12 +0700

    tweaks

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const stripBom = require('strip-bom');
 const parseJson = require('parse-json');
 const pify = require('pify');
 
-const parse = (x, fp) => parseJson(stripBom(x), path.relative(process.cwd(), fp));
+const parse = (data, fp) => parseJson(stripBom(data), path.relative('.', fp));
 
 module.exports = fp => pify(fs.readFile)(fp, 'utf8').then(data => parse(data, fp));
 module.exports.sync = fp => parse(fs.readFileSync(fp, 'utf8'), fp);
--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 import path from 'path';
 import test from 'ava';
-import fn from './';
+import m from './';
 
 const fixture = path.join(__dirname, 'package.json');
 
 test('async', async t => {
-	const data = await fn(fixture);
+	const data = await m(fixture);
 	t.is(data.name, 'load-json-file');
 });
 
 test('sync', t => {
-	t.is(fn.sync(fixture).name, 'load-json-file');
+	t.is(m.sync(fixture).name, 'load-json-file');
 });
