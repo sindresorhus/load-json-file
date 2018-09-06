c95f14fbefb6c9b566836765f65cc76f6f61a23b
Denis <carriere.denis@gmail.com>
2018-09-06 00:06:29 -0600

    Add index.test-d.ts

---
+++ index.test-d.ts
@@ -0,0 +1,16 @@
+import {expectType} from 'tsd-check';
+import loadJsonFile, {sync, Reviver, BeforeParse} from '.';
+
+(async () => {
+	// To-Do => Expand Tests
+	// https://github.com/sindresorhus/write-json-file/blob/master/index.test-d.ts
+	expectType<Reviver>(() => 1);
+	expectType<Reviver>((a: string) => a.length);
+	expectType<Reviver>((a: string, b: string) => a.length - b.length);
+
+	expectType<BeforeParse>((data) => data);
+
+	expectType<void>(await loadJsonFile('unicorn.json'));
+
+	expectType<void>(sync('unicorn.json'));
+})();
