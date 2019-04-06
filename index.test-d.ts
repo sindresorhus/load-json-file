a9f653966221a2dab460942ebeb5dee3aa4b2345
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-06 14:25:17 +0200

    Refactor TypeScript definition to CommonJS compatible export

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,12 +1,14 @@
-import {expectType} from 'tsd-check';
-import loadJsonFile, {sync, Reviver, BeforeParse} from '.';
+import {expectType} from 'tsd';
+import {JsonValue} from 'type-fest';
+import loadJsonFile = require('.');
+import {Reviver, BeforeParse} from '.';
 
 expectType<Reviver>(() => 1);
 expectType<Reviver>((a: string) => a.length);
 expectType<Reviver>((a: string, b: string) => a.length - b.length);
 
-expectType<BeforeParse>((data) => data);
+expectType<BeforeParse>(data => data);
 
-expectType<unknown>(await loadJsonFile('unicorn.json'));
+expectType<Promise<JsonValue>>(loadJsonFile('unicorn.json'));
 
-expectType<unknown>(sync('unicorn.json'));
+expectType<JsonValue>(loadJsonFile.sync('unicorn.json'));
